0f7ca7b2e354a3202a53a10f435352095126bfba
brismed3118 <rmbenito@iesrafaelalberti.es>
2024-09-30 21:34:05 +0200

    V30/09.2 commit

--- src/ej17.py
+++ src/ej17.py
@@ -6,5 +6,6 @@ nom = input("Hola! Cómo te llamas?: ")
 
 num = int(input("Ahora dime las veces que quieres que se repita tu nombre: "))
 
-for i in range (0, num):
-    print(nom)
+nom = (nom + "\n") * num
+
+print(nom)
