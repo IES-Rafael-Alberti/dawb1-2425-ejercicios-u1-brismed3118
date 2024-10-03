c16c99ff0c152e59b14c3ca6c35451113a83ebd9
brismed3118 <rmbenito@iesrafaelalberti.es>
2024-10-03 21:20:24 +0200

    "Added code to split and print birthdate components"

--- src/ej25.py
+++ src/ej25.py
@@ -3,3 +3,7 @@
 #
 
 nacimiento = input("Introduce el día de tu nacimiento en formato dd/mm/aaaa: ")
+
+dia, mes, año =  nacimiento.split("/")
+
+print(f"Naciste el {dia} en el mes {mes} del año {año}")
